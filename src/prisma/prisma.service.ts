9029667fd929e1a7f64dd9a8800528ad3bd10d13
hsn0najafi <hsn0najafi@gmail.com>
2022-06-14 11:50:53 +0430

    feat(): postgres and prisma setup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            datasources: {
+                db: {
+                    url: 'postgresql://__test_user__:__test_password__@localhost:5432/__test_db__?schema=public',
+                },
+            },
+        });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
+}
