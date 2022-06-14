72694d39f0fef9a6cb8dbec5a9d3d1125f586391
hsn0najafi <hsn0najafi@gmail.com>
2022-06-14 12:04:42 +0430

    feat(): swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 (async () => {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('nest-auth-crud-prisma')
+        .setDescription('nest-auth-crud-prisma')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(8000);
 })();
